92d1184bae7f1e176464d46aef0baab6cd1dab30
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-05 19:34:31 +0100

    Replace homepage in package-json two

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 import axios from 'axios';
 import App from './src/App.js';
 
-axios.defaults.baseURL =  "http://localhost:8000"; 
-//axios.defaults.baseURL =  "https://app-project-jczo.onrender.com";
+//axios.defaults.baseURL =  "http://localhost:8000"; 
+axios.defaults.baseURL =  "https://app-project-jczo.onrender.com";
 
 
 
