3e105e2b8d8ad8a14c29541be1a529a09abeda32
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-05 17:54:23 +0100

    Change BrowserRouter for HashRouter

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import axios from 'axios';
 import App from './src/App.js';
 
-axios.defaults.baseURL =  "https://app-project-jczo.onrender.com" || "http://localhost:8000"; //"https://app-project-jczo.onrender.com" ||
+//axios.defaults.baseURL =  "http://localhost:8000"; 
+axios.defaults.baseURL =  "https://app-project-jczo.onrender.com";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
-		<BrowserRouter>
+		<HashRouter>
 					
 				<App />		
 			
-		</BrowserRouter>
+		</HashRouter>
 	);
