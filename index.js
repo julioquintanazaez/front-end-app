4122e516deaf60384df8723a9dca57530d147ab0
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-06 02:21:31 +0100

    add new config

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
 	
-        <BrowserRouter>
+        
             <App1 />			
-        </BrowserRouter>
+        
     
 	);
