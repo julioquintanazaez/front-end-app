7d5161b43ddbc0239f37d15324472f3f183ee8c1
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-06 02:00:28 +0100

    add new config

--- index.js
+++ index.js
@@ -8,21 +8,10 @@ import axios from 'axios';
 //import App from './src/App.js';
 
 //axios.defaults.baseURL =  "http://localhost:8000"; 
-axios.defaults.baseURL =  "https://app-project-jczo.onrender.com";
+//axios.defaults.baseURL =  "https://app-project-jczo.onrender.com";
 
-const App1 = () => {
+const App1 = () => {	
 	
-	const [token, setToken] = useState("");
-	const [user, setUser] = useState({});
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	
-	const user_logged = { id: '1', name: 'julio', role: 'admin' };	
-	//&& user_logged.role.includes('admin')
-	/*
-	<Route element={<ProtectedRoute isAllowed={ isLoggedIn } />}>
-		<Route path="/dashboard" element={<MainDashboard />} />
-	</Route>
-	*/
 	return (
 		<>		
 			<h1> Hello world from App1 .....</h1>
