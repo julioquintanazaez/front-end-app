231626eb396151fe16f9a1480cefa4042b7d3969
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-07 04:44:30 +0100

    removin browserrouting and add homepage site with name

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const App1 = () => {
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
-		<BrowserRouter >					
+						
 			<App />				
-		</BrowserRouter>
+		
     
 	);
