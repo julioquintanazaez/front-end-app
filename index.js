b04176c68fcea4610319fad7bb73b3c5e0d9dc67
Julio C Quintana Zaez <jupyterdevs2022@gmail.com>
2024-01-07 04:19:16 +0100

    change homepage

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const App1 = () => {
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
-		<HashRouter>					
+		<BrowserRouter>					
 			<App />				
-		</HashRouter>
+		</BrowserRouter>
     
 	);
